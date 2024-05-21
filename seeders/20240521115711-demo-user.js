cfcba346b5f72225a8f0d4b03fe4f4e695b6e504
muhammaddani <muhammaddani308@gmail.com>
2024-05-21 21:08:46 +0700

    seeders dan perbaikan nama file

---
+++ seeders/20240521115711-demo-user.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [{
+      email: 'kel6@gmail.com',
+      password: await bcrypt.hash('kel6',10),
+      name: 'Kelompok Enam',
+      nim: '2211520000',
+      role:'mahasiswa',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      email: 'admin@gmail.com',
+      password: await bcrypt.hash('admin',10),
+      name: 'admin',
+      nim: '',
+      role:'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }
+    ],{});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
